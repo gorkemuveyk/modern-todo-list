45ef69d4389ad2962dd78d6f50552c3b1b943ee3
Gorkem Uveyk <uveykgorkem@gmail.com>
2023-03-03 00:27:21 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-// VARIABLES
+// HTML ELEMENT VARIABLES
 let tasks = document.querySelectorAll(".task-item");
 const taskText = document.querySelector("#text-input");
 const taskBtn = document.querySelector("#btn-input");
 const deleteBtn = document.querySelector(".trash");
-const editBtn = document.querySelector("#edit");
 const ul = document.querySelector(".tasks");
 const alertMessage = document.querySelector(".alert-message");
+
+// DATE TIME VARIABLES
 const date = new Date();
 const year = date.getFullYear();
 const month =
   date.getMonth() + 1 < 10 ? "0" + date.getMonth() : date.getMonth();
 const day = date.getDay() < 10 ? "0" + date.getDay() : date.getDay();
-const hour = date.getHours();
-const minute = date.getMinutes();
+const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+const minute =
+  date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
 
-console.log(taskText.value);
+//KEYBOARD ENTER TRIGGER
+function searchKeyPress(e) {
+  e = e || window.event;
+  if (e.keyCode == 13) {
+    taskBtn.click();
+    return false;
+  }
+  return true;
+}
+
 // ADD BTN CLICK
 taskBtn.addEventListener("click", (e) => {
   if (taskText.value != "") {
@@ -31,25 +42,43 @@ taskBtn.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
-deleteBtn.addEventListener("click", (e) => {
-  deleteLiElement(e.target.parentElement.parentElement);
-});
+ul.addEventListener("click", deleteLiElement);
 
 // FUNCTIONS
 function createLiElement(val) {
   return `
-  <li class="task-item">
+  <li class="task-item ">
   <span class="task-time">${day}.${month}.${year} - ${hour}:${minute}</span>
   ${val}
   <span class="icons">
-    <i id="trash" class="fa-solid fa-trash"></i>
-    <i id="edit" class="fa-solid fa-pen"></i>
+    <i id="trash" class="trash fa-solid fa-trash "></i>
   </span>
   </li>
 `;
 }
+let count = 0;
 function deleteLiElement(element) {
-  console.log(element);
+  const item = element.target;
+  // DELETE TODO
+  if (item.classList[0] === "trash") {
+    if (count % 2 === 0) {
+      element.target.parentElement.parentElement.classList.add(
+        "animate__animated",
+        "animate__backOutRight"
+      );
+      count++;
+    } else {
+      element.target.parentElement.parentElement.classList.add(
+        "animate__animated",
+        "animate__backOutLeft"
+      );
+      count++;
+    }
+
+    setTimeout(() => {
+      element.target.parentElement.parentElement.style.display = "none";
+    }, 400);
+  }
 }
 // SHOW ALERT MESSAGE
 function showMessage() {
